ee8ef4e19d6218c2cfb0cd39a3a75c3ed8807f55
DEVA <divyanshuchaudhary482@gmail.com>
2025-02-18 14:23:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ import random
 
 # Bot details from environment variables
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Environment variable se token le raha hai #Your bot token 
-CHANNEL_1_USERNAME = "𝐓𝐅𝐖 ⌯ 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 •"  # First channel username
-CHANNEL_2_USERNAME = "𝗧𝗙𝗪 ✘ 𝗨𝗣𝗗𝗔𝗧𝗘𝗦"  # Second channel username
+CHANNEL_1_USERNAME = "TFW_NETWORK"  # First channel username
+CHANNEL_2_USERNAME = "TFW_UPDATES"  # Second channel username
 API_HASH = "59ee8005ce6b056fa639d956f028eeeb"
 API_ID = "25614292"
 
